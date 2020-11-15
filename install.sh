e4a8a00c5a50a79fe59dafef251c094b87f3566a
Arnaud Weyts <arnaud.weyts@gmail.com>
2020-11-15 23:04:33 +0100

    feat(git): add symlink commands for git

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# git
+echo "Configuring git..."
+ln -fs git/.gitconfig $HOME/.gitconfig
+ln -fs git/.gitignore $HOME/.gitignore
+
 # vscode
 echo "Configuring vscode..."
 ln -fs VSCode/settings.json $HOME/Library/Application Support/Code/User/settings.json
