11411e1e7c48e50e4ad61c4256cad7fa9f5a4de2
Arnaud Weyts <arnaud.weyts@gmail.com>
2020-11-15 23:01:22 +0100

    feat(vscode): add symlink commands for settings and keybinds

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 # vscode
 echo "Configuring vscode..."
+ln -fs VSCode/settings.json $HOME/Library/Application Support/Code/User/settings.json
+ln -fs VSCode/keybindings.json $HOME/Library/Application Support/Code/User/keybindings.json
 cat extensions.list | xargs -L 1 code --install-extension
