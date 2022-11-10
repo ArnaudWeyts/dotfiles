cd7a1bbcd8c09297ac98b7ca4878df757f267058
Arnaud Weyts <arnaud.weyts@gmail.com>
2022-11-10 13:35:27 +0100

    feat(fish): update configuration for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,5 @@
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/arnaudweyts/google-cloud-sdk/path.fish.inc' ]; . '/Users/arnaudweyts/google-cloud-sdk/path.fish.inc'; end
