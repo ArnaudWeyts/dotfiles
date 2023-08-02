a9f90a3384d7433288a8d3fc60687d63f66b7113
Arnaud Weyts <arnaud.weyts@gmail.com>
2023-08-02 17:03:59 +0200

    feat(fish): update for python

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,6 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/arnaudweyts/google-cloud-sdk/path.fish.inc' ]; . '/Users/arnaudweyts/google-cloud-sdk/path.fish.inc'; end
+
+pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
